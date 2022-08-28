0e27298c7adaaf40eb2b06a68a470440c158bfee
Mark Eaton <MarkEEaton@users.noreply.github.com>
2022-08-28 14:14:40 -0400

    Find It menu working perfectly

--- configuration/main.js
+++ configuration/main.js
@@ -1,11 +1,9 @@
 const menusComponent = {
   template:
-  `<a class="searchmenu" v-bind:aria-label="item.description" v-bind:href="item.link">
-     <div class="bigger-fancy-text">
-        <i v-bind:class="item.icon" aria-hidden="true"></i>
-        <strong>{{ item.description }}</strong>
-     </div>
-   </a>`,
+  `<div class="bigger-fancy-text">
+    <i v-bind:class="item.icon" aria-hidden="true"></i>
+    <strong>{{ item.description }}</strong>
+  </div>`,
    props: ['item'] 
 }
 
